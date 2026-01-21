2e03b0d67ebe08fff097ffb02628e8ece9bea356
neg1t <laze24@mail.ru>
2026-01-21 17:44:58 +0700

    feat: ko

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import react from "@vitejs/plugin-react";
+import { vitePluginVersionMark } from "vite-plugin-version-mark";
+
+const getVersionVariable: () => Plugin = () => ({
+  name: "version-variable",
+  config(config) {
+    // get version in vitePlugin if you open `ifGlobal`
+    console.log(config.define);
+    return defineConfig({
+      define: {
+        "import.meta.env.APP_VERSION": JSON.stringify(
+          config.define?.__TEST_APP_VERSION__
+            ?.replaceAll(/"/g, "")
+            .replaceAll('"', "") || "0.0.0",
+        ),
+      },
+    });
+  },
+});
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginVersionMark({
+      name: "test-app",
+      // version: '0.0.1',
+      command: "git describe --tags --always",
+      outputFile: (version) => ({
+        path: "src/shared/version/version.json",
+        content: `{"version":"${version}"}`,
+      }),
+      // ifGitSHA: true,
+      // ifShortSHA: true,
+      // ifMeta: true,
+      ifLog: true,
+      ifGlobal: true,
+    }),
+    getVersionVariable(),
+  ],
   server: {
     port: 8000,
   },
