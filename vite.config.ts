11261bd8ab5f2ced58fee30088e9a88bfa31e983
neg1t <laze24@mail.ru>
2026-01-23 02:22:46 +0700

    fix: try without version display

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, type Plugin } from "vite";
 import react from "@vitejs/plugin-react";
-import { vitePluginVersionMark } from "vite-plugin-version-mark";
+// import { vitePluginVersionMark } from "vite-plugin-version-mark";
 
 const getVersionVariable: () => Plugin = () => ({
   name: "version-variable",
@@ -23,20 +23,20 @@ const getVersionVariable: () => Plugin = () => ({
 export default defineConfig({
   plugins: [
     react(),
-    vitePluginVersionMark({
-      name: "test-app",
-      // version: '0.0.1',
-      command: "git describe --tags --always",
-      outputFile: (version) => ({
-        path: "src/shared/version/version.json",
-        content: `{"version":"${version}"}`,
-      }),
-      // ifGitSHA: true,
-      // ifShortSHA: true,
-      // ifMeta: true,
-      ifLog: true,
-      ifGlobal: true,
-    }),
+    // vitePluginVersionMark({
+    //   name: "test-app",
+    //   // version: '0.0.1',
+    //   command: "git describe --tags --always",
+    //   outputFile: (version) => ({
+    //     path: "src/shared/version/version.json",
+    //     content: `{"version":"${version}"}`,
+    //   }),
+    //   // ifGitSHA: true,
+    //   // ifShortSHA: true,
+    //   // ifMeta: true,
+    //   ifLog: true,
+    //   ifGlobal: true,
+    // }),
     getVersionVariable(),
   ],
   server: {
